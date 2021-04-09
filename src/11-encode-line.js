7ce815fd7dd140cce5ee514f39ad60aea666780a
AnnaKoliada <anacirec@gmail.com>
2021-04-09 20:18:52 +0300

    feat: add some tests

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,30 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let arr = [];
+  arr = str.split('');
+  const result = [];
+  let el = 1;
+  for (let i = 0; i < arr.length;) {
+    if (arr[i] !== arr[i + 1]) {
+      result.push(arr[i]);
+    }
+    if (arr[i] === arr[i + 1]) {
+      el++;
+      let j = i;
+      for (j = i; j < arr.length; j++) {
+        if (arr[j + 1] === arr[j + 2]) {
+          el++;
+          i++;
+        } else break;
+      }
+      i++;
+      result.push(el + arr[i]);
+    }
+    i++;
+    el = 1;
+  } return result.join('');
 }
 
 module.exports = encodeLine;
